660f2389ac23bda63a5a191bd57caecd2911bbc4
YazdanPlug <73750616+YazdanPlug@users.noreply.github.com>
2020-11-16 13:28:24 +0330

    Update ClearChat.java

--- src/Main/ir.yzplug.ClearChat/ClearChat.java
+++ src/Main/ir.yzplug.ClearChat/ClearChat.java
@@ -1,10 +1,10 @@
 package ir.yzplug.ClearChat;
 
+import cn.nukkit.Player;
+import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.Server;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
-import cn.nukkit.Player;
-import cn.nukkit.plugin.PluginBase;
 
 public class ClearChat extends PluginBase {
     @Override
@@ -12,29 +12,25 @@ public class ClearChat extends PluginBase {
         this.saveDefaultConfig();
         this.getLogger().info("This Plugin Enabled!");
     }
-    
-    @Override
-    public void onDisable() { this.getLogger().info("This Plugin Disabled!"); 
-                            }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
-            Player player = (Player)sender;
-            if (player.hasPermission("clearchat.use")) {
+            Player p = (Player)sender;
+            if (p.hasPermission("clearchat.use")) {
                 for(int i = 0; i < 100; ++i) {
                     Server.getInstance().broadcastMessage("");
                 }
-                Server.getInstance().broadcastMessage(this.getConfig().getString("Chat-Cleared").replace("%player%", player.getDisplayName()));
+                Server.getInstance().broadcastMessage(this.getConfig().getString("Chat-Cleared").replace("{player}", p.getDisplayName()));
                 return true;
             } else {
-                player.sendMessage(this.getConfig().getString("No-Permission-Message"));
+                p.sendMessage(this.getConfig().getString("You don't have Premission this Command"));
                 return false;
             }
         } else {
             for(int i = 0; i < 100; ++i) {
                 Server.getInstance().broadcastMessage("");
             }
-            Server.getInstance().broadcastMessage(this.getConfig().getString("Cleared-By-Console"));
+            Server.getInstance().broadcastMessage(this.getConfig().getString("Cleared by Console"));
             return true;
         }
     }
